bf7809ae5c478641a37b179d4fc961907f77d019
Raphael Margueron <raphael.margueron@gmail.com>
2026-03-01 00:52:51 +0100

    update cd with fabric script and rsync

--- fabfile.py
+++ fabfile.py
@@ -2,18 +2,30 @@
 
 import click
 import fabric
+import requests
 
 
 def deploy(c):
+    REPO = "TheRaphael0000/ch.tahm-ken.www"
+    releases = requests.get(
+        f"https://api.github.com/repos/{REPO}/releases").json()
+    versions = [r["tag_name"] for r in releases]
+    for v in versions:
+        print(f"- {v}")
+    version = click.prompt(
+        "Version to deploy",
+        type=click.Choice(versions),
+        show_choices=False,
+        default=versions[0]
+    )
+    release_url = f"https://github.com/{REPO}/releases/download/{version}/tahm-ken.ch-{version}.tar.gz"
+
     with c.cd("/opt/ch.tahm-ken.www"):
-        c.run("git fetch")
-        c.run("git status")
-        if not click.confirm("Stash and deploy to main ?", default=True):
-            exit()
-        c.run("git stash")
-        c.run("git checkout main")
-        c.run("git pull")
-        c.run("VITE_BUILD_VERSION=$(git describe --tags | tr -d '\n') docker compose up --build")
+        has_version = c.run(f"test -d {version}", warn=True).ok
+        if not has_version:
+            c.run(f"mkdir -p {version}")
+            c.run(f"curl -L {release_url} | tar -xzf - -C {version}")
+        c.run(f"rsync -av --delete --progress --size-only {version}/ live/")
 
 
 c = fabric.Connection(host="tahm-ken.ch", user="root", port=22)
